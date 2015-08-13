ce1f1aaca63f95e31de82ed7c4c4acfb61dfaeff
Mary Elisabeth <melemmer@gmail.com>
2015-08-13 09:26:38 -0700

    Made changes to copy for home page and sign up page

---
+++ src/app/new-subscriber/new-subscriber.js
@@ -0,0 +1,23 @@
+angular.module( 'ngBoilerplate.new-garden', [
+  'ui.router',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'new-garden', {
+    url: '/new-garden',
+    views: {
+      "main": {
+        controller: 'NewGardenCtrl',
+        templateUrl: 'new-garden/new-garden.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'New Garden' }
+  });
+})
+
+.controller( 'NewGardenCtrl', function NewGardenCtrl( $scope ) {
+
+})
+
+;
