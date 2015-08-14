c1dd6e3d9ffa88b2958dde1ba3490abddf84e8a9
Allxie <allxiecleary@gmail.com>
2015-08-14 16:21:53 -0700

    connected profile info to angular. Minor style fixes.

--- src/app/show-garden/show-garden.js
+++ src/app/show-garden/show-garden.js
@@ -18,10 +18,26 @@ angular.module( 'ngBoilerplate.show-garden', [
 })
 
 .controller( 'ShowGardenCtrl', function ShowGardenCtrl( $scope ) {
+  
 
-  $scope.foobar = "Hello from the show-garden.js";
-
-  $scope.gardenImages = ["assets/images/community-2.png","assets/images/community-1.jpeg","assets/images/community-3.jpeg"]
+  // Profile page info
+  $scope.scapeName = "Mary's Foodscape";
+  $scope.status = "Hello from the show-garden.js. This will be a status message.";
+  $scope.username = "Mary L.";
+  $scope.gardenImages = ["assets/images/community-2.png","assets/images/community-1.jpeg","assets/images/community-3.jpeg"];
+  // This stuff goes in the white box under the orange labels
+  $scope.location = "The Sunset, San Francisco, CA";
+  $scope.produce = "veggies, fruites, etc.";
+  $scope.goalsAndNeeds = "I need a hoe. I'm going to build a planter box.";
+  $scope.otherDetails = "I have a friendly dog.";
+  $scope.updates = [{
+                      "date": "4/15/15"
+                    , "content": "Watered today."
+                    }
+                    ,{
+                      "date": "5/30/15"
+                    , "content": "I planted tomatoes!"
+                    }]
 
 })
 
