103cfd5699006cbfe7b3792a719278be88de4788
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-24 15:09:28 -0700

    fix main column in pictures migration

--- db/migrate/20150815211922_create_pictures.rb
+++ db/migrate/20150815211922_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
       t.references :foodscape
-      t.main :boolean
+      t.boolean :main
 
       t.timestamps null: false
     end
