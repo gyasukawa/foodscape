e99d10263eca8835252196c0c7f18d083efb29f4
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-24 15:31:27 -0700

    update pictures column

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20150822004218) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
-    t.integer  "zip_code"
+    t.integer  "zip_code",               default: 0,  null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
