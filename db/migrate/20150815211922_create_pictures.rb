a860e1e5e33e525f40806ca295c548c614d08e79
Irene Ma <iring.ma@gmail.com>
2015-08-23 18:39:45 -0700

    initial commit

--- db/migrate/20150815211922_create_pictures.rb
+++ db/migrate/20150815211922_create_pictures.rb
@@ -1,7 +1,9 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-
+      t.references :foodscape
+      t.main :boolean
+      t.
       t.timestamps null: false
     end
   end
