0cb856ea49909071a0d6d90eaadbae4044b5a5a9
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-26 16:44:01 -0700

    change callbacks to omniauth_callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,20 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
+      # user.provider = auth.provider
+      # user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      # user.image = auth.info.image # assumes the user profile is an image attribute on the user model, to add in when we figure out paperclip
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 
