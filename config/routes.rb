7e6711fa52e2547289f5ed431e4d6a4c1233692b
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-26 14:10:50 -0700

    add following index to users controller and custom route: users/:id/following

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'users/:id/following' => 'users#following', as :following
   post 'foodscapes/:id/follow' => 'foodscapes#follow', as: :follow
   delete 'foodscapes/:id/unfollow' => 'foodscapes#unfollow', as: :unfollow
 
