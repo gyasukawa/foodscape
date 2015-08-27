9a6a1582644f1f15de7cab70cc3705122ffe4d11
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-26 18:33:01 -0700

    add avatar column to users migration file and add dev file path to convert installation on local machine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
 
   # Default url options for devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  Paperclip.options[:command_path] = "/opt/local/bin/"
 end
