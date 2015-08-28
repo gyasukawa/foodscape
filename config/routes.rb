cf6ec8a8872a6a59e0750f32aa59539377bd5210
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-08-27 20:47:34 -0700

    changed current_user controller action name and route to the_current_user so this isn't confused with devise's current_user method

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   get 'users/:id/following' => 'users#following', as: :following
   post 'foodscapes/:id/follow' => 'foodscapes#follow', as: :follow
   delete 'foodscapes/:id/unfollow' => 'foodscapes#unfollow', as: :unfollow
-  get 'current_user' => 'users#current_user', as: :current_user
+  get 'the_current_user' => 'users#the_current_user', as: :the_current_user
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :foodscapes do
