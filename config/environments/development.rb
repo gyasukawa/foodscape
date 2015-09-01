dc44ad270261075bdeeaa43f3e8b5195a92a40b5
Jeremy J Barth <jeremy.barth@wellnessfx.com>
2015-09-01 14:57:48 -0700

    use s3 for paperclip storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,12 @@ Rails.application.configure do
   # Default url options for devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  Paperclip.options[:command_path] = "/opt/local/bin/"
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => foodscape.development,
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
 end
